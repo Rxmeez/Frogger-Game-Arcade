6139ff164cc8da34f737ace92c3e8748f8641ba6
Rameez Khan <Rameez@campus-10-41-76-250.wireless.leeds.ac.uk>
2017-01-28 17:49:34 +0000

    Colour Added & ran through JSHint and JSBeautifer

--- js/app.js
+++ js/app.js
@@ -1,72 +1,61 @@
-// Enemies our player must avoid
+//Global variable for movement for Player defined as char and Enemey defined as enemy
 var charXMove = 101;
 var charYMove = 83;
-var enemyXPosition = 101;
+var enemyXPosition = [101, 150, 221];
 var enemyYPosition = [60, 140, 220];
+var charEnemyWidth = 80;
+var charEnemyHeight = 50;
+var defaultCharPosition = [200, 400];
 
-
+// Every Enemy has this basic starting point
 var Enemy = function() {
-
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-  this.x = -enemyXPosition;
-  this.y = enemyYPosition[Math.floor(Math.random()*3)];
-  this.width = 80;
-  this.height= 50;
-  this.speed = Math.floor(Math.random()*20)+200
+    this.sprite = 'images/enemy-bug.png';
+    this.x = -enemyXPosition[Math.floor(Math.random() * 3)];
+    this.y = enemyYPosition[Math.floor(Math.random() * 3)];
+    this.width = charEnemyWidth;
+    this.height = charEnemyHeight;
+    this.speed = Math.floor((Math.random() * 100) + 200);
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//Enemy object as time continues
 Enemy.prototype.update = function(dt) {
 
-  if (this.x > 500){
-    this.x = -enemyXPosition;
-    this.y = enemyYPosition[Math.floor(Math.random()*3)];
-  }else{
-    this.x = this.x + (dt*this.speed)
-  }
+    if (this.x > 500) {
+        this.x = -enemyXPosition[Math.floor(Math.random() * 3)];
+        this.y = enemyYPosition[Math.floor(Math.random() * 3)];
+    } else {
+        this.x = this.x + (dt * this.speed);
+    }
 
-
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
+// drawing Enemy on the canvas, with this.x and this.y position
 Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(){
-  this.sprite ='images/char-boy.png';
-  this.x = 200;
-  this.y = 400;
-  this.width = 80;
-  this.height = 50;
-  this.score = 0;
+//Basic Settings for Player when its spawned
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = defaultCharPosition[0];
+    this.y = defaultCharPosition[1];
+    this.width = charEnemyWidth;
+    this.height = charEnemyHeight;
+    this.score = 0;
+    document.getElementById('score').innerHTML = this.score;
 
 };
 
-Player.prototype.update = function(keypress){
+Player.prototype.update = function(keypress) {
 
 };
 
-Player.prototype.render = function(){
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
+//Player being rendered on the canvas
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 var player = new Player();
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 
@@ -75,78 +64,78 @@ var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
 
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
 
 Player.prototype.handleInput = function(keys) {
 
-  switch(keys){
-    case 'left':
-    var leftPosition = this.x - 100;
-    if (leftPosition >= 0){
-      this.x = leftPosition;
-    };
-    break;
-    case 'right':
-    var rightPosition = this.x + 100;
-    if (rightPosition <= 400){
-      this.x = rightPosition;
-    };
-    break;
-    case 'up':
-    var upPosition = this.y - 82;
-    if (upPosition >= -82){
-      this.y = upPosition;
-    };
-    break;
-    case 'down':
-    var downPosition = this.y + 82;
-    if (downPosition <= 400){
-      this.y = downPosition;
-    };
-    break;
-    default:
-    console.log("Can't go that way");
-  };
+    switch (keys) {
+        case 'left':
+            var leftPosition = this.x - charXMove;
+            if (leftPosition >= -2) { //canvas max left position
+                this.x = leftPosition;
+            }
+            break;
+        case 'right':
+            var rightPosition = this.x + charXMove;
+            if (rightPosition <= 402) { //canvas max right position
+                this.x = rightPosition;
+            }
+            break;
+        case 'up':
+            var upPosition = this.y - charYMove;
+            if (upPosition >= -83) { //canvas max top position
+                this.y = upPosition;
+            }
+            break;
+        case 'down':
+            var downPosition = this.y + charYMove;
+            if (downPosition <= 450) { //canvas max bottom postion
+                this.y = downPosition;
+            }
+            break;
+        default:
+            console.log("Can't go that way");
+    }
 };
 
-Player.prototype.update = function(dt){
-  this.checkCollisions();
-if (this.y < 0){
-  this.score += 20;
-console.log("Win!");
-console.log(this.score);
-this.x = 400/2;
-this.y = 400;
-var scoreCard = document.getElementById('score');
-scoreCard.innerHTML = this.score;
-}else{
-  this.score = this.score
-}
-}
+Player.prototype.update = function(dt) {
+    this.checkCollisions();
+    if (this.y < 0) {
+        this.score += 20;
+        console.log("Win!");
+        console.log(this.score);
+        this.x = defaultCharPosition[0];
+        this.y = defaultCharPosition[1];
 
-Player.prototype.reset = function(){
-  this.x = 400/2;
-  this.y = 400;
-  this.score = 0
-  document.getElementById('score').innerHTML = this.score;
-  console.log("reset");
-}
+        //Adding Score to HTML DOM
+        document.getElementById('score').innerHTML = this.score;
+    } else {
+        this.score = this.score;
+    }
+};
 
-Player.prototype.checkCollisions = function(){
+Player.prototype.reset = function() {
+    this.x = defaultCharPosition[0];
+    this.y = defaultCharPosition[1];
+    this.score = 0;
+    document.getElementById('score').innerHTML = this.score;
+    console.log("reset");
+};
 
-for(i = 0; i < allEnemies.length; i++)
-  if ((this.x <= allEnemies[i].x + allEnemies[i].width) && (this.x + this.width >= allEnemies[i].x) && (this.y < allEnemies[i].y + allEnemies[i].height) && (this.y + this.height > allEnemies[i].y)){
-    console.log("COLLISION");
+Player.prototype.checkCollisions = function() {
 
-    this.reset();
-  };
+    for (i = 0; i < allEnemies.length; i++)
+        if ((this.x <= allEnemies[i].x + allEnemies[i].width) && (this.x + this.width >= allEnemies[i].x) && (this.y < allEnemies[i].y + allEnemies[i].height) && (this.y + this.height > allEnemies[i].y)) {
+            console.log("COLLISION");
+            this.reset();
+        }
 
-}
+};
